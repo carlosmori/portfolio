cd2235b681bec99f3569ea015601756f0642ffaf
Carlos Mori <cmori.94@gmail.com>
2022-07-14 16:43:12 -0300

    feat: refactor about page

--- portfolio/components/About.tsx
+++ portfolio/components/About.tsx
@@ -33,12 +33,37 @@ function About() {
   setTimeout(() => {
     setBoolean(true)
   }, 2000)
-  useEffect(() => {
-    console.log(`aboutContainerRef equals:`)
-    console.log(aboutContainerRef.current.offsetTop)
-    console.log(`scrollY equals:`)
-    console.log(scrollY)
-  }, [aboutContainerRef.current, scrollY])
+  const shouldDisplayAboutMe = () => {
+    // If we are scrolling in at least 33% of the About Page I should Start displaying the item
+    return scrollY > (aboutContainerRef.current?.offsetTop * 2) / 3 - width
+  }
+  const shouldElementBeFixedInTheMiddleOfThePage = () => {
+    // Check if the element surpases half of the viewport width
+    return (
+      ((aboutContainerRef.current?.offsetTop * 2) / 3) * -1 + scrollY >
+      viewportWidth / 2 - width / 2
+    )
+  }
+  const getHalfOfTheViewportCordenates = () => {
+    // returns the middle of the viewport width amount in pixels
+    return `${viewportWidth / 2 - width / 2}px`
+  }
+  const hideElement = () => {
+    // Hides the element with a negative left position
+    return `-${width}px`
+  }
+  const keepScrollingRight = () => {
+    return `${
+      ((aboutContainerRef.current?.offsetTop * 2) / 3) * -1 + scrollY
+    }px`
+  }
+  const shouldDisplayAboutMeDescription = () => {
+    return (
+      ((aboutContainerRef.current?.offsetTop * 2) / 3) * -1 + scrollY >
+      viewportWidth / 2 - width
+    )
+  }
+
   return (
     <div
       ref={aboutContainerRef}
@@ -48,25 +73,18 @@ function About() {
         ref={aboutH1Ref}
         className={`absolute top-[25%] -left-[-${width}px] from-neutral-400 font-oswald text-6xl tracking-widest opacity-5 md:text-9xl`}
         style={{
-          left:
-            scrollY > (aboutContainerRef.current?.offsetTop * 2) / 3 - width
-              ? ((aboutContainerRef.current?.offsetTop * 2) / 3) * -1 +
-                  scrollY >
-                viewportWidth / 2 - width / 2
-                ? `${viewportWidth / 2 - width / 2}px`
-                : `${
-                    ((aboutContainerRef.current?.offsetTop * 2) / 3) * -1 +
-                    scrollY
-                  }px`
-              : `-${width}px`,
+          left: shouldDisplayAboutMe()
+            ? shouldElementBeFixedInTheMiddleOfThePage()
+              ? getHalfOfTheViewportCordenates()
+              : keepScrollingRight()
+            : hideElement(),
         }}
       >
         ABOUT ME
       </h2>
       <div
         className={`text-md text-center  font-sans text-2xl ${
-          ((aboutContainerRef.current?.offsetTop * 2) / 3) * -1 + scrollY >
-          viewportWidth / 2 - width
+          shouldDisplayAboutMeDescription()
             ? 'animate-fade-in'
             : 'animate-fast-fade-out opacity-0'
         }`}
